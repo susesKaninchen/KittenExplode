0d869c78e8ebb8a7ff30d1f51a368577aad14680
Andre S <info@snake4you.de>
2023-03-10 23:59:17 +0100

    umbau auf timer

--- RandomShit/src/main.cpp
+++ RandomShit/src/main.cpp
@@ -28,15 +28,19 @@ const uint8_t brightness = 60;
 int isr_flag = 0;
 char displaybuffer[4] = {' ', ' ', ' ', ' '};
 
-int buttonState = 0;
+int buttonStateL = 0;
+int buttonStateR = 0;
 
-void handleGesture() {
-    uint8_t gesture = apds.readGesture();
-    if(gesture == APDS9960_DOWN) Serial.println("v");
-    if(gesture == APDS9960_UP) Serial.println("^");
-    if(gesture == APDS9960_LEFT) Serial.println("<");
-    if(gesture == APDS9960_RIGHT) Serial.println(">");
-}
+unsigned long timerRainbow, timerFillHex, timerFillMatrix, timerBar, timerGesture;
+int modeMatrix = 1;
+int modeBar = 1;
+int countBar, countMatrix;
+boolean flagBar = false;
+
+const int buzzChan = 0;
+
+
+
 
 /*
 void interruptRoutine() {
@@ -77,8 +81,15 @@ void setup() {
 	pinMode(PIN_PUSH_4, INPUT);
 	pinMode(PIN_GESTURE_INT_MATRIX, INPUT_PULLUP);
 
+  ledcSetup(buzzChan, 2000, 12);
+  ledcAttachPin(PIN_BUZZER, buzzChan);
   //attachInterrupt(PIN_GESTURE_INT_MATRIX, interruptRoutine, FALLING);
 
+  ledcWriteTone(buzzChan, 600);
+  delay(500);
+  ledcWriteTone(buzzChan, 800);
+  delay(500);
+  ledcWriteTone(buzzChan, 0);
 
 	FastLED.addLeds<UCS1903, PIN_LED_MATRIX, RGB>(ledsRGB, getRGBWsize(NUM_LEDS));
   FastLED.addLeds<WS2811, PIN_LED_RING>(ledRing, 10);
@@ -105,13 +116,12 @@ void setup() {
   //apds.setProximityInterruptThreshold(0, 175);
   //enable the proximity interrupt
   //apds.enableProximityInterrupt();
-  //apds.setGestureGain(APDS9960_GGAIN_4);
+  apds.setGestureGain(APDS9960_GGAIN_8);
 
   ina219.setADCMode(SAMPLE_MODE_128);
   ina219.setMeasureMode(CONTINUOUS);
   ina219.setPGain(PG_80);
   ina219.setBusRange(BRNG_16);
-  //ina219.setShuntVoltOffset(-0.02)
 
   alpha4.writeDigitRaw(3, 0x0);
   alpha4.writeDigitRaw(0, 0xFFFF);
@@ -140,24 +150,16 @@ void setup() {
   }
   bar.writeDisplay();
   delay(2000);
+}
 
+void handleGesture() {
+    uint8_t gesture = apds.readGesture();
+    if(gesture == APDS9960_DOWN) Serial.println("v");
+    if(gesture == APDS9960_UP) Serial.println("^");
+    if(gesture == APDS9960_LEFT) Serial.println("<");
+    if(gesture == APDS9960_RIGHT) Serial.println(">");
 }
-void colorFill(CRGB c){
-	for(int i = 0; i < NUM_LEDS; i++){
-		leds[i] = c;
-		FastLED.show();
-		delay(20);
-	}
-	delay(200);
-}
-void fillWhite(){
-	for(int i = 0; i < NUM_LEDS; i++){
-		leds[i] = CRGBW(0, 0, 0, 255);
-		FastLED.show();
-		delay(20);
-	}
-	delay(200);
-}
+
 void rainbow(){
 	static uint8_t hue;
 	for(int i = 0; i < 10; i++){
@@ -166,14 +168,7 @@ void rainbow(){
 	FastLED.show();
 	hue++;
 }
-void rainbowLoop(){
-	long millisIn = millis();
-	long loopTime = 2000; // 5 seconds
-	while(millis() < millisIn + loopTime){
-		rainbow();
-		delay(5);
-	}
-}
+
 void writehex() {
   char c = random(255);
     if (!isprint(c)) {
@@ -196,31 +191,106 @@ void writehex() {
  
   // write it out!
   alpha4.writeDisplay();
+}
 
-  rainbowLoop();
-  //handleGesture();
+void barLoop() {
+  if (countBar < 24) {
+    if (flagBar) {
+      bar.setBar(countBar, LED_OFF);
+      flagBar = false;
+      countBar++;
+    } else {
+      switch (modeBar) {
+      case 1:
+        bar.setBar(countBar, LED_GREEN);
+        break;
+      case 2:
+        bar.setBar(countBar, LED_RED);
+        break;
+      case 3:
+        bar.setBar(countBar, LED_YELLOW);
+        break;
+      default:
+        bar.setBar(countBar, LED_GREEN);
+        modeBar = 1;
+        break;
+      }
+      flagBar = true;
+    }
+    bar.writeDisplay();
+  }
+  if (countBar >= 24) {
+    countBar = 0;
+    modeBar++;
+  }
+}
+
+void colorFill() {
+  if (countMatrix < NUM_LEDS) {
+    switch (modeMatrix) {
+      case 1:
+        leds[countMatrix] = CRGB::Red;
+        break;
+      case 2:
+        leds[countMatrix] = CRGB::Green;
+        break;
+      case 3:
+        leds[countMatrix] = CRGB::Blue;
+        break;
+      case 4:
+        leds[countMatrix] = CRGBW(0, 0, 0, 255);
+        break;
+      default:
+        leds[countMatrix] = CRGB::Red;
+        modeMatrix = 1;
+        break;
+    }
+		FastLED.show();
+    countMatrix++;
+  } else {
+    countMatrix = 0;
+    modeMatrix++;
+  }
+}
+
+void fillWhite(){
+	for(int i = 0; i < NUM_LEDS; i++){
+		leds[i] = CRGBW(0, 0, 0, 255);
+		FastLED.show();
+		delay(20);
+	}
+	delay(200);
 }
 
 void loop() {
-  /*
-  if(!digitalRead(PIN_GESTURE_INT_MATRIX)){
-    Serial.println(apds.readProximity());
-    Serial.println("Interrupt loop called!");
-    //handleGesture();
-    //clear the interrupt
-    apds.clearInterrupt();
+  if (timerFillHex < millis()) {
+    timerFillHex = millis() + 500;
+    writehex();
   }
-  buttonState = digitalRead(PIN_TOUCH_L);
-  if (buttonState == HIGH) {
-    // turn LED on:
+  if (timerRainbow < millis()) {
+    timerRainbow = millis() + 10;
+    rainbow();
+  }
+  if (timerBar < millis()) {
+    timerBar = millis() + 50;
+    barLoop();
+  }
+  if (timerFillMatrix < millis()) {
+    timerFillMatrix = millis() + 50;
+    colorFill();
+  }
+  if (timerGesture < millis()) {
+    timerGesture = millis() + 10;
+    handleGesture();
+  }
+  
+  buttonStateL = digitalRead(PIN_TOUCH_L);
+  if (buttonStateL == HIGH) {
     digitalWrite(PIN_MOT_EN, HIGH);
   } else {
-    // turn LED off:
     digitalWrite(PIN_MOT_EN, LOW);
   }
-  */
-  //handleGesture();
-  //delay(500);
+
   /*
   float shuntVoltage_mV = 0.0;
   float loadVoltage_V = 0.0;
@@ -248,45 +318,5 @@ void loop() {
     Serial.println("Overflow! Choose higher PGAIN");
   }
   Serial.println();
-  
-  delay(500);
   */
-  //if( isr_flag == 1 ) {
-    //detachInterrupt(PIN_GESTURE_INT_MATRIX);
-    //Serial.println("Interrupt called!");
-    //isr_flag = 0;
-    //attachInterrupt(PIN_GESTURE_INT_MATRIX, interruptRoutine, FALLING);
-  //}
-  
-  
- for (uint8_t b=0; b<24; b++) {
-   bar.setBar(b, LED_RED);
-   bar.writeDisplay();
-   delay(50);
-   bar.setBar(b, LED_OFF);
-   bar.writeDisplay();
- }
- writehex();
- colorFill(CRGB::Red);
-  for (uint8_t b=0; b<24; b++) {
-   bar.setBar(b, LED_GREEN);
-   bar.writeDisplay();
-   delay(50);
-   bar.setBar(b, LED_OFF);
-   bar.writeDisplay();
- }
-writehex();
-colorFill(CRGB::Green);
- for (uint8_t b=0; b<24; b++) {
-   bar.setBar(23-b, LED_YELLOW);
-   bar.writeDisplay();
-   delay(50);
-   bar.setBar(23-b, LED_OFF);
-   bar.writeDisplay();
- }
-writehex();
-colorFill(CRGB::Blue);
-fillWhite();
-
-  //handleGesture();
 }
